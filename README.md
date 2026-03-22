c65d5cdbf5ad1ca829ceefde903ced22bd0a1750
iainfox <furghadmanar@gmail.com>
2026-03-22 17:33:01 -0500

    Ui concept

--- README.md
+++ README.md
@@ -1,7 +1,4 @@
-# Tauri + Vanilla TS
+# Peptide Tracker
+This app helps users manage and track people's peptide schedules with ease. It’s designed for individuals taking multiple peptides with varying dosages and complex or irregular intervals, providing a clear and organized way to stay consistent and on schedule.
 
-This template should help get you started developing with Tauri in vanilla HTML, CSS and Typescript.
-
-## Recommended IDE Setup
-
-- [VS Code](https://code.visualstudio.com/) + [Tauri](https://marketplace.visualstudio.com/items?itemName=tauri-apps.tauri-vscode) + [rust-analyzer](https://marketplace.visualstudio.com/items?itemName=rust-lang.rust-analyzer)
+It is a mobile application that uses the [tauri](https://v2.tauri.app/) framework to 
--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,56 @@
-import { invoke } from "@tauri-apps/api/core";
+const title = document.getElementById("date-title")
 
-let greetInputEl: HTMLInputElement | null;
-let greetMsgEl: HTMLElement | null;
+function getDaySuffix(date: Date) {
+    const day = date.getDate()
+    if (day >= 11 && day <= 13) return `${day}th`
 
-async function greet() {
-  if (greetMsgEl && greetInputEl) {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    greetMsgEl.textContent = await invoke("greet", {
-      name: greetInputEl.value,
-    });
-  }
+    const lastDigit = day % 10
+    const suffix = { 1: "st", 2: "nd", 3: "rd" }[lastDigit] || "th"
+
+    return `${day}${suffix}`
 }
 
-window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form")?.addEventListener("submit", (e) => {
-    e.preventDefault();
-    greet();
-  });
-});
+if (title) {
+    const today = new Date()
+
+    const day = String(today.getDate())
+    const day_name = getDaySuffix(today)
+    const month = String(today.getMonth() + 1).padStart(2, '0')
+    const month_name = today.toLocaleString('default', { month: 'long' })
+    const year = today.getFullYear()
+
+    title.innerHTML = `<time datetime='${year}-${month}-${day}'>${month_name} ${day_name}: ${month}/${day}</time>`
+}
+
+document.querySelectorAll('input[type="checkbox"]').forEach(element => {
+    const checkbox = element as HTMLInputElement;
+    checkbox.addEventListener("change", (_) => {
+        const parent = checkbox.closest('li')
+        if (!parent) return
+
+        const radio = parent.querySelector('input[type="radio"]') as HTMLInputElement | null
+        if (!radio) return
+
+        if (!checkbox.checked) {
+            parent.classList.remove('light')
+            radio.removeAttribute("disabled")
+            return
+        }
+
+        parent.classList.add('light')
+        radio.setAttribute("disabled", '')
+    })
+})
+
+document.querySelectorAll('input[type="radio"]').forEach(element => {
+    const radio = element as HTMLInputElement;
+    radio.addEventListener('change', (_) => {
+        const group = document.querySelectorAll(`input[name="${radio.name}"]`)
+
+        group.forEach(r => {
+            r.closest('li')?.classList.remove('selected')
+        })
+
+        radio.closest('li')?.classList.add('selected')
+    });
+})
