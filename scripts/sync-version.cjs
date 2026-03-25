8dcfde8b4893eb4abbde18d6cd34c1390f5b06f4
iainfox <furghadmanar@gmail.com>
2026-03-24 20:27:49 -0500

    fix es module error and tauri v1 json version location

--- scripts/sync-version.cjs
+++ scripts/sync-version.cjs
@@ -3,7 +3,7 @@ const fs = require("fs");
 const pkg = JSON.parse(fs.readFileSync("package.json", "utf8"));
 const tauri = JSON.parse(fs.readFileSync("src-tauri/tauri.conf.json", "utf8"));
 
-tauri.package.version = pkg.version;
+tauri.version = pkg.version;
 
 fs.writeFileSync(
   "src-tauri/tauri.conf.json",
