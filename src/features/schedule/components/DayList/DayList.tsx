d2e04b191be40720e11606b091c4c5aa4ed74b35
iainfox <furghadmanar@gmail.com>
2026-04-04 00:36:29 -0500

    add container styling

--- src/features/schedule/components/DayList/DayList.tsx
+++ src/features/schedule/components/DayList/DayList.tsx
@@ -20,19 +20,33 @@ export function DayList({
     setCheckedItems,
 }: Props) {
     return (
-        <div>
-            {/* date header later */}
+        <div className={"peptides"}>
+            {(() => {
+                const now = new Date();
+                const monthLong = now.toLocaleString('default', { month: 'long' });
+                const monthNum = String(now.getMonth() + 1).padStart(2, '0');
+                const dayNum = String(now.getDate()).padStart(2, '0');
+                return (
+                    <h2 className="list-title">
+                        {`${monthLong}, ${monthNum}/${dayNum}`}
+                    </h2>
+                );
+            })()}
 
-            {sections.map((section) => (
-                <TimeSection
-                    key={section.time}
-                    section={section}
-                    selectedItemId={selectedItemId}
-                    onSelectItem={onSelectItem}
-                    checkedItems={checkedItems}
-                    setCheckedItems={setCheckedItems}
-                />
-            ))}
+            <ul className="day-group">
+                {sections.map((section) => (
+                    <li>
+                        <TimeSection
+                            key={section.time}
+                            section={section}
+                            selectedItemId={selectedItemId}
+                            onSelectItem={onSelectItem}
+                            checkedItems={checkedItems}
+                            setCheckedItems={setCheckedItems}
+                        />
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
