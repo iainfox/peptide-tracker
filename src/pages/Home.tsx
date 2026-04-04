5b7806ca4d3f9c64f4b0d9c88f0a994d9e602f1c
iainfox <furghadmanar@gmail.com>
2026-04-03 23:54:24 -0500

    Fix global def where it was supposed to be scoped

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,27 +1,27 @@
 import { useState } from "react";
-import { Navbar } from "../components/Navbar/Navbar";
-import { DayList } from "../features/schedule/components/DayList";
+import Navbar from "../components/Navbar/Navbar";
+import { DayList } from "../features/schedule/components/DayList/DayList";
 import "./Home.css"
 
-const [checkedItems, setCheckedItems] = useState<Set<number>>(new Set());
-
-const data = [
-    {
-        time: "9:00 AM",
-        items: [
-            { id: 1, name: "Task A" },
-            { id: 2, name: "Task B" },
-        ],
-    },
-    {
-        time: "10:00 AM",
-        items: [
-            { id: 3, name: "Task C" },
-        ],
-    },
-];
-
 function Home() {
+    const [checkedItems, setCheckedItems] = useState<Set<number>>(new Set());
+
+    const data = [
+        {
+            time: "9:00 AM",
+            items: [
+                { id: 1, name: "Task A" },
+                { id: 2, name: "Task B" },
+            ],
+        },
+        {
+            time: "10:00 AM",
+            items: [
+                { id: 3, name: "Task A" },
+            ],
+        },
+    ];
+
     return <div>
         <h1>Home Page</h1>
         <DayList
