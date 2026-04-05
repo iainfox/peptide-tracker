261a763857a682acba8ff8216c73256646462ac2
iainfox <furghadmanar@gmail.com>
2026-04-05 17:38:40 -0500

    pass children to component

--- src/features/add-form/components/InputArea/InputArea.tsx
+++ src/features/add-form/components/InputArea/InputArea.tsx
@@ -1,23 +1,22 @@
-import { ReactElement } from "react";
-import "./InputArea.css"
+import { ReactElement, ReactNode } from "react";
+import "./InputArea.css";
 
 type Props = {
     icon: ReactElement;
     title: string;
-}
+    children?: ReactNode;
+};
 
-function InputArea({
-    icon,
-    title
-}: Props) {
+function InputArea({ icon, title, children }: Props) {
     return (
         <div className="input-area">
             <div className="input-area-title">
                 {icon}
                 <h2>{title}</h2>
             </div>
+            <div className="input-area-content">{children}</div>
         </div>
     );
 }
 
-export default InputArea
+export default InputArea;
